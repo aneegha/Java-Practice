44df2f252b5ced3ad302bd0859d0eaaf28c2c423
Aneegha Brilly <aneeghabrilly13@gmail.com>
2026-01-08 02:54:05 +0530

    Create BankAccount.java

---
+++ OOPs/Encapsulation/BankAccount.java
@@ -0,0 +1,44 @@
+public class BankAccount {
+
+    private String accountHolderName;
+    private long accountNumber;
+    private double balance;
+
+    public BankAccount(String accountHolderName, long accountNumber, double balance) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: ₹" + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: ₹" + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount");
+        }
+    }
+
+    public void displayAccountInfo() {
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: ₹" + balance);
+    }
+}
